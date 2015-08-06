4ed653262d07d364f500cd023030a61edad801cf
Judy Long <long.judy92@gmail.com>
2015-08-06 19:30:18 -0400

    Fix Game#play and #switch_player

--- game.rb
+++ game.rb
@@ -11,9 +11,22 @@ class Game
   end
 
   def switch_player
+    self.current_player = current_player == :B ? :W : :B
   end
 
   def play
+    board.render
+    until board.over?
+      puts "#{current_player}'s turn: "
+      input = get_move
+
+      seq = parse(input)
+      raise InvalidMoveError if !board.on_board?(seq[0]) ||
+                    board[seq[0]].nil? || board[seq[0]].color != current_player
+      board[seq[0]].perform_moves(*seq.drop(1))
+      board.render
+      switch_player
+    end
   end
 
   def get_move
@@ -23,7 +36,7 @@ class Game
   end
 
   def parse(input)
-    string_sequence = string_sequence.split(".").map(&:strip)
+    string_sequence = input.split(" ").map(&:strip)
     seq = string_sequence.map{|arr| arr[1...-1].split(",").map(&:to_i)}
   end
 end
