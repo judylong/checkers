99aef0ba521e72d9f3a66d806c377663b761f169
Judy Long <long.judy92@gmail.com>
2015-08-06 18:07:29 -0400

    Add Game#initialize, #switch_player, and #play

--- game.rb
+++ game.rb
@@ -1,4 +1,19 @@
 require_relative 'board'
 
 class Game
+  attr_accessor :board, :current_player
+
+  def initialize
+    @board = Board.new
+    board.fill_board
+    @current_player = :B
+
+  end
+
+  def switch_player
+  end
+
+  def play
+  end
+
 end
