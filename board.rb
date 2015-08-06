a72d239ac97347227dceab93e8da964e8a438d6a
Judy Long <long.judy92@gmail.com>
2015-08-06 10:00:39 -0400

    Add Board#initialize and bracket methods

--- board.rb
+++ board.rb
@@ -1,4 +1,22 @@
 require_relative 'piece'
 
 class Board
+  SIZE = 8
+
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new(SIZE) { Array.new(SIZE) }
+  end
+
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, obj)
+    row, col = pos
+    grid[row][col] = obj
+  end
+  
 end
