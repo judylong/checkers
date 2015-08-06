3e9d08bcaccea421bbe597c9aac42886fea7db24
Judy Long <long.judy92@gmail.com>
2015-08-06 10:16:05 -0400

    Add promoted instance variable to Piece class and Piece#promte and #promoted? methods

--- piece.rb
+++ piece.rb
@@ -2,11 +2,20 @@ require_relative 'board'
 
 class Piece
   attr_accessor :pos, :color
+  attr_reader :promoted
 
   def initialize(pos, color)
       @pos = pos
       @color = color
+      @promoted = false
   end
 
+  def promoted?
+    promoted
+  end
+
+  def promote
+    @promoted = true
+  end
 
 end
