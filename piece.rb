4f351b1e2d5ebe4ed26318b58efd7fefa97704dc
Judy Long <long.judy92@gmail.com>
2015-08-06 11:10:49 -0400

    Add Piece#perform_slide, #perform_jump, #move_diffs and DELTAS constant

--- piece.rb
+++ piece.rb
@@ -1,13 +1,19 @@
 require_relative 'board'
 
 class Piece
-  attr_accessor :pos, :color
+  DELTAS = [
+    [1, 1],
+    [1, -1]
+  ]
+
+  attr_accessor :pos, :color, :board
   attr_reader :promoted
 
-  def initialize(pos, color)
+  def initialize(pos, color, board)
       @pos = pos
       @color = color
       @promoted = false
+      @board = board
   end
 
   def promoted?
@@ -17,5 +23,32 @@ class Piece
   def promote
     @promoted = true
   end
+
+  def perform_slide(new_pos)
+    return false if !board.on_board?(new_pos) || !board[new_pos].nil?
+    row, col = pos
+    moves = DELTAS.map { |dr, dc| [row + (dr*move_diffs), col + (dc*move_diffs)]}
+    valid_pos = new_pos if moves.include?(new_pos)
+    board[pos] = nil
+    board[valid_pos] = self
+    true
+  end
+
+  def inspect
+    {"p"}
+  end
+
+  def perform_jump(pos)
+    return false if illegal?(pos)
+    row, col = pos
+
+    true
+  end
+
+  def move_diffs
+    color == :B ? 1 : -1
+  end
+
+
 
 end
